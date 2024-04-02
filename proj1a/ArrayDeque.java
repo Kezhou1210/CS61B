5c38557a53d3648b6a6d65c906b7738785bf3fdc
Kezhou <118445887+Kezhou1210@users.noreply.github.com>
2024-04-02 11:35:07 +0800

    circular arraydeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,42 +1,68 @@
 public class ArrayDeque<T> {
     private T[] items;
     private int size;
+    private int nextLast;
+    private int nextFirst;
+    private int length;
 
     public ArrayDeque(){
-        items =(T[]) new Object[8];
+        items = (T[]) new Object[8];
         size = 0;
+        nextLast = 5;
+        nextFirst = 4;
+        length = 8;
     }
 
-    private void resize(int capacity){
+    private void grow(){
+        int capacity = length * 2;
         T[] temp = (T[]) new Object[capacity];
-        System.arraycopy(items, 0, temp, 0, size);
+        System.arraycopy(items, 0 , temp, capacity/4, size);
         items = temp;
+        nextFirst = capacity/4 -1;
+        nextLast = 3*capacity/4;
+        length = capacity;
+    }
+
+    private void shrink(){
+        int capacity = length/2;
+        T[] temp = (T[]) new Object[capacity];
+        if(nextFirst > nextLast){
+            System.arraycopy(items, nextFirst+1-length, temp, capacity/4,size);
+        }else{
+            System.arraycopy(items, nextFirst +1, temp, capacity/4 ,size);
+        }
+        items = temp;
+        nextFirst = capacity/4 -1;
+        nextLast = 3*capacity/4;
+        length = capacity;
     }
 
     public void addFirst(T item){
-        if(size == items.length){
-            resize(size * 2);
+        if(size == length){
+            grow();
         }
-        T[] temp = (T[]) new Object[items.length];
-        System.arraycopy(items, 0, temp, 1,size);
-        temp[0] = item;
-        items = temp;
+        items[nextFirst] = item;
         size++;
+        nextFirst--;
+        if(nextFirst <0){
+            nextFirst += length;
+        }
     }
 
-    public void addLast(T x){
-        if(size == items.length){
-            resize(size * 2);
+    public void addLast(T item){
+        if(size == length){
+            grow();
         }
-        items[size] = x;
+        items[nextLast] = item;
         size++;
+        nextLast++;
+        if(nextLast > length-1){
+            nextLast = length - nextLast;
+        }
     }
 
     public boolean isEmpty(){
-        if (size == 0) {
-            return true;
-        }
-        return false;
+        return size==0;
     }
 
     public int size(){
@@ -44,48 +70,54 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque(){
-        for(int i = 0; i<size; i++){
-            System.out.print(items[i]+" ");
+        int start;
+        if(nextFirst > nextLast){
+            start = nextFirst +1 -length;
+        }else{
+            start = nextFirst + 1;
         }
-        System.out.println(items[size]+" ");
-    }
-
-    public T removeFirst(){
-        if(size == 0){
-            return null;
+        for(int i = start; i<start+size;i++){
+            System.out.print(items[i] + " ");
         }
-        T x = items[0];
-        T[] temp = (T[]) new Object[items.length];
-        System.arraycopy(items,1,temp,0,size -1);
-        items = temp;
-        size--;
-        if(items.length/size >= 4){
-            resize(items.length/2);
-        }
-        return x;
-    }
-
-    private T getLast(){
-        return items[size-1];
+        System.out.println("");
     }
 
     public T removeLast(){
+        if(size/length <= 0.25 && length >= 16){
+            shrink();
+        }
         if(size ==0){
             return null;
         }
-        T x = getLast();
         size--;
-        if(items.length/size >= 4){
-            resize(items.length/2);
+        nextLast--;
+        if(nextLast <0){
+            nextLast = nextLast + length;
         }
-        return x;
+        T item = items[nextLast];
+        return item;
+    }
+
+    public T removeFirst(){
+        if(size/length <= 0.25 && length >= 16){
+            shrink();
+        }
+        if(size ==0){
+            return null;
+        }
+        size--;
+        nextFirst++;
+        if(nextFirst > length - 1){
+            nextFirst -= length;
+        }
+        T item = items[nextFirst];
+        return item;
     }
 
     public T get(int index){
-        if(index > size){
-            return null;
+        if(index + nextFirst +1 >length){
+            return items[index+nextFirst+1-length];
         }
-        return items[index];
+        return items[index + nextFirst +1];
     }
-
 }
